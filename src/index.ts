65d70fed82f7de9e040bf9827d25c0577988b909
alexandr <alexander.avdeev@balance-pl.ru>
2021-05-17 19:44:29 +0300

    example 5 is ready

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-console.log('here run');
+interface PageInfo {
+    title: string;
+}
+
+type PageKey = 'about' | 'contact' | 'home';
+
+const x: Record<PageKey, PageInfo> = {
+    about: {
+        title: 'sd',
+    },
+    contact: {
+        title: '123123v xfz c',
+    },
+    home: {
+        title: 'sda',
+    },
+};
