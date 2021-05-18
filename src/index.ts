b43747e3a404c817c5b7dab74165b63cfa937e63
alexandr <alexander.avdeev@balance-pl.ru>
2021-05-18 08:18:19 +0300

    example 6 "infer" is ready

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,4 @@
-console.log('here run');
+type ElementType2<T> = T extends (infer U)[] ? U : T;
+type B = ElementType2<number[]>; // number
+type C = ElementType2<string[]>; // string
+type E = ElementType2<boolean[]>; // boolean
