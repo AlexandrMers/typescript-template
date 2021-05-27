4b4a3612be233d9925d2295ef9c20d6df9f846bb
Алекандр Авдеев <alexander.avdeev@balance-pl.ru>
2021-05-27 19:33:56 +0300

    finish example of Option type

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,44 @@
 "use strict";
-console.log("typescript is here)");
+// определим тип Option
+// определим класс Some, который является подтипом Option
+class Some {
+    constructor(value) {
+        this.value = value;
+    }
+    flatMap(f) {
+        return f(this.value);
+    }
+    getOrElse() {
+        return this.value;
+    }
+}
+// определим класс None, который также является подтипом Option
+// tslint:disable-next-line:max-classes-per-file
+class None {
+    flatMap() {
+        return this;
+    }
+    getOrElse(value) {
+        return value;
+    }
+}
+function option(value) {
+    if (value == null) {
+        return new None();
+    }
+    return new Some(value);
+}
+const result = option(6)
+    .flatMap((value) => option(value.toString()))
+    .flatMap((value) => new None())
+    .getOrElse('error'); // В данном случае ts четко понимает, что будет выведен текст 'error'
+// tslint:disable-next-line:no-console
+console.log('result 1', result);
+const result2 = option('text')
+    .flatMap((value) => option(Number(value)))
+    .flatMap((value) => new Some(value.toString()))
+    .flatMap((value) => new Some(3))
+    .getOrElse(); // В данном случае ts не требует введения значения по умолчанию, потому что четко понимает, что возвращается number
+// tslint:disable-next-line:no-console
+console.log('result 2', result2);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,58 @@
-console.log('here run');
+// определим тип Option
+
+interface Option<T> {
+    flatMap<U>(f: (value: T) => None): None;
+    flatMap<U>(f: (value: T) => Option<U>): Option<U>;
+    getOrElse(value: T): T;
+}
+
+// определим класс Some, который является подтипом Option
+class Some<T> implements Option<T> {
+    constructor(private value: T) {}
+    flatMap<U>(f: (value: T) => None): None;
+    flatMap<U>(f: (value: T) => Some<U>): Some<U>;
+    flatMap<U>(f: (value: T) => Option<U>): Option<U> {
+        return f(this.value);
+    }
+    getOrElse(): T {
+        return this.value;
+    }
+}
+
+// определим класс None, который также является подтипом Option
+// tslint:disable-next-line:max-classes-per-file
+class None implements Option<never> {
+    flatMap(): None {
+        return this;
+    }
+    getOrElse<U>(value: U): U {
+        return value;
+    }
+}
+
+// Осталось реализовать функцию Option, здесь также будем использовать перегрузку сигнатуры:
+function option<T>(value: null | undefined): None; // *1
+function option<T>(value: T): Some<T>; // *2
+function option<T>(value: T): Option<T> {
+    if (value == null) {
+        return new None();
+    }
+    return new Some(value);
+}
+
+const result = option(6)
+    .flatMap((value) => option(value.toString()))
+    .flatMap((value) => new None())
+    .getOrElse('error'); // В данном случае ts четко понимает, что будет выведен текст 'error'
+
+// tslint:disable-next-line:no-console
+console.log('result 1', result);
+
+const result2 = option('text')
+    .flatMap((value) => option(Number(value)))
+    .flatMap((value) => new Some(value.toString()))
+    .flatMap((value) => new Some(3))
+    .getOrElse(); // В данном случае ts не требует введения значения по умолчанию, потому что четко понимает, что возвращается number
+
+// tslint:disable-next-line:no-console
+console.log('result 2', result2);
